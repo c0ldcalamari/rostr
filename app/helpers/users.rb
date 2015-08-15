0205149a4c30e5aa3a895e3159b8ebb0318dd38c
Andrew Pham & Zoe Ingram <pair+atp1o2.zoeingram@devbootcamp.com>
2015-08-15 16:49:25 -0700

    routes for organizer

---
+++ app/helpers/users.rb
@@ -0,0 +1,20 @@
+helpers do
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout!
+    session[:user_id] = nil
+  end
+
+  def current_user
+    @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+
+end
