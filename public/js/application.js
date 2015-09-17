bd46fcfe6438f37386c2c004e4b6938ba652930c
atp1o2 <andrewpham8@gmail.com>
2015-09-16 17:15:42 -0700

    fix routes

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
+  // This is called after the document has loaded in its entirety
+  // This guarantees that any elements we bind to will exist on the page
+  // when we try to bind to them
 
-  // $.ajax({
-  //   url: this.settings.chatEndPoint,
-  //   type: 'post',
-  //   dataType: 'json',
-  //   data: {
-  //     'chat_info': data
-  //   },
+  showSignup();
+  showLogin();
+  cancel();
 });
+
+
+var showSignup = function(){
+  $("#signup").on('click', function(event){
+    $(".button").hide();
+    $("#signup-form").show();
+  })
+}
+
+var showLogin = function(){
+  $("#login").on('click', function(event){
+    $(".button").hide();
+    $("#login-form").show();
+  })
+}
+
+var cancel = function(){
+  $(".cancel").on('click', function(event){
+    event.preventDefault();
+    $(".button").show();
+    $(".form").hide();
+  })
+}
